569fd3be4b0382cc47901a39c7ed6e5538f8e15f
Kunyuuu <kan@fandm.edu>
2020-10-13 16:46:47 +0800

    edited data part

--- credit_ranking/logistic.py
+++ credit_ranking/logistic.py
@@ -6,13 +6,14 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 import matplotlib.pyplot as plt
 
+os.chdir(r"C:\Users\longcredit\Projects\InternProject\credit_ranking")
 pd.set_option('display.max_columns', None)
 
-#read data and data cleaning
+#data cleaning and preprocessing
 accepts = pd.read_csv('accepts.csv', skipinitialspace = True)
 accepts = accepts.dropna(axis = 0, how = 'any')
 
 #cross table
-cross_table = pd.crosstab(accepts.banltruptcy_ind, 
-                            accepts.bad_ind, margins=True)
+cross_table = pd.crosstab(accepts.used_ind,accepts.bad_ind, margins=True)
 cross_table
+
